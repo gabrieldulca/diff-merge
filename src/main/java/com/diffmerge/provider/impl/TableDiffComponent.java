40df5bce3b8d3b8a1ec02540c58bd6fd05128f9a
irina.avram <XzDWDJVS>
2020-04-05 15:56:39 +0200

    Individual components for tree and table

---
+++ src/main/java/com/diffmerge/provider/impl/TableDiffComponent.java
@@ -0,0 +1,42 @@
+package main.java.com.diffmerge.provider.impl;
+
+import java.util.List;
+
+import org.eclipse.emf.compare.Comparison;
+import org.eclipse.emf.compare.Diff;
+import org.eclipse.glsp.graph.GModelRoot;
+
+import main.java.com.diffmerge.dto.ComparisonDto;
+import main.java.com.diffmerge.dto.DiffDto;
+import main.java.com.diffmerge.exception.InvalidParametersException;
+import main.java.com.diffmerge.mapper.TableDiffMapper;
+import main.java.com.diffmerge.mapper.DiffMapper;
+import main.java.com.diffmerge.provider.DiffComponent;
+
+public class TableDiffComponent extends DiffComponent{
+	
+	public TableDiffComponent() {
+		super.setType("table");
+	}
+	
+	@Override
+	public ComparisonDto getComparison(String model1, String model2, String model3) throws InvalidParametersException {
+				
+		Comparison comparison = compare(model1, model2, model3);
+
+		List<Diff> diffList = comparison.getDifferences();
+		ComparisonDto comparisonDto = new ComparisonDto();
+		
+		DiffMapper mapper = new TableDiffMapper();
+		
+		for(Diff diff:diffList) {
+			DiffDto diffDto = mapper.toDiffDto(diff);
+			if(diffDto != null) {
+				comparisonDto.addDifference(diffDto);
+			}
+		}
+		return comparisonDto;
+		
+	}
+
+}
